7f6a4bfcf78ad4c05554eb5c4e1d03b9e32f54d6
abdul-ruet <abdul.ruet@gmail.com>
2024-10-01 03:07:22 +0600

    function

---
+++ script.js
@@ -0,0 +1,47 @@
+// Login Button event handler
+
+const loginBtn = document.getElementById("login");
+      loginBtn.addEventListener("click", function () {
+        const loginArea = document.getElementById("login-area");
+        loginArea.style.display = "none";
+        const transactionArea = document.getElementById("transaction-area");
+        transactionArea.style.display = "block";
+      });
+
+// deposit button
+
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener('click', function(){
+    const depositAmount = document.getElementById('depositAmount').value;
+    const depositNumber= parseFloat(depositAmount);
+
+
+    // const currentDeposit = document.getElementById('current-deposit').innerText;
+    // const currentDepositNumber = parseFloat(currentDeposit);
+
+    // const totalDeposit = depositNumber + currentDepositNumber;
+    // // console.log(totalDeposit);
+    // document.getElementById('current-deposit').innerText = totalDeposit;
+    updateSpanText("current-deposit", depositNumber)
+
+    document.getElementById('depositAmount').value = "";
+    updateSpanText("current-balance", depositNumber)
+
+    // const currentBalance = document.getElementById('current-balance').innerText;
+    // const currentBalanceNumber = parseFloat(currentBalance);
+    // const totalBalance = depositNumber + currentBalanceNumber ;
+    // document.getElementById("current-balance").innerText = totalBalance;
+
+    document.getElementById("depositAmount").value = "";
+
+
+})
+
+function updateSpanText(id, depositNumber){
+  const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber ;
+    document.getElementById(id).innerText = totalAmount;
+
+
+}
